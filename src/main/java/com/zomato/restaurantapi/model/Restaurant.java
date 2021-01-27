e37b8d9a19dffb1b3eba30218cf30e4dd53f039b
Amol Grover <amol.grover@zomato.com>
2021-01-27 17:20:50 +0530

    Add Restaurant model

---
+++ src/main/java/com/zomato/restaurantapi/model/Restaurant.java
@@ -0,0 +1,48 @@
+package com.zomato.restaurantapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private boolean isDineInAvailable;
+    private int numberOfSeats;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDineInAvailable() {
+        return isDineInAvailable;
+    }
+
+    public void setDineInAvailable(boolean dineInAvailable) {
+        isDineInAvailable = dineInAvailable;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+}
