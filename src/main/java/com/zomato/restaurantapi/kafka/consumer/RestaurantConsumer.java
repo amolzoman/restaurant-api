9b8ba18edb9491406d59442872c32f5a066134bd
Amol Grover <amol.grover@zomato.com>
2021-01-29 12:20:29 +0530

    Use consumer service to add records to database

--- src/main/java/com/zomato/restaurantapi/kafka/consumer/RestaurantConsumer.java
+++ src/main/java/com/zomato/restaurantapi/kafka/consumer/RestaurantConsumer.java
@@ -2,9 +2,11 @@ package com.zomato.restaurantapi.kafka.consumer;
 
 import com.zomato.restaurantapi.kafka.utils.KafkaUtils;
 import com.zomato.restaurantapi.model.Restaurant;
+import com.zomato.restaurantapi.kafka.service.RestaurantConsumerServiceImpl;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +16,12 @@ public class RestaurantConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(RestaurantConsumer.class);
 
+    @Autowired
+    private RestaurantConsumerServiceImpl restaurantConsumerService;
+
     @KafkaListener(topics = TOPIC)
     public void consume(ConsumerRecord<String, Restaurant> consumerRecord) {
         log.info("Consuming " + consumerRecord.value());
+        restaurantConsumerService.handleAddNewRestaurant(consumerRecord.value());
     }
 }
