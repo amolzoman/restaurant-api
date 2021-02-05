7ea3d97847cac57f3ab61b3e46cea39f3fcdd025
Amol Grover <amol.grover@zomato.com>
2021-02-05 10:29:39 +0530

    Modify controller to use the new service instead of directly accessing DAO

--- src/main/java/com/zomato/restaurantapi/controller/RestaurantController.java
+++ src/main/java/com/zomato/restaurantapi/controller/RestaurantController.java
@@ -1,9 +1,7 @@
 package com.zomato.restaurantapi.controller;
 
-import com.zomato.restaurantapi.exception.RestaurantNotFoundException;
-import com.zomato.restaurantapi.kafka.service.RestaurantProducerServiceImpl;
 import com.zomato.restaurantapi.model.Restaurant;
-import com.zomato.restaurantapi.repository.RestaurantRepository;
+import com.zomato.restaurantapi.service.RestaurantServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,71 +12,49 @@ import java.util.List;
 @RequestMapping(path = "/api/v1")
 public class RestaurantController {
     @Autowired
-    private RestaurantProducerServiceImpl restaurantProducerService;
-
-    @Autowired
-    private RestaurantRepository restaurantRepository;
+    private RestaurantServiceImpl restaurantService;
 
     @GetMapping(path = "/restaurants")
     public List<Restaurant> getAllRestaurants() {
-        return restaurantRepository.findAll();
+        return restaurantService.findAll();
     }
 
     @GetMapping(path = "/restaurants/{id}")
     public Restaurant getRestaurantById(@PathVariable Long id) {
-        return getRestaurantByIdOrThrow(id);
+        return restaurantService.findById(id);
     }
 
     @GetMapping(path = "/restaurants", params = "name")
     public Restaurant getRestaurantByName(@RequestParam String name) {
-        return restaurantRepository.findByName(name)
-                .orElseThrow(() -> new RestaurantNotFoundException(name));
+        return restaurantService.findByName(name);
     }
 
     @GetMapping(path = "/restaurants", params = "dineInAvailable")
     public List<Restaurant> getRestaurantsByDineInAvailability(@RequestParam boolean dineInAvailable) {
-        return restaurantRepository.findByIsDineInAvailable(dineInAvailable)
-                .orElseThrow(() -> new RestaurantNotFoundException(dineInAvailable));
+        return restaurantService.findByDineInAvailability(dineInAvailable);
     }
 
     @PostMapping(path = "/restaurants")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void addNewRestaurant(@RequestBody Restaurant restaurant) {
-        restaurantProducerService.handleAddNewRestaurant(restaurant);
+        restaurantService.save(restaurant);
     }
 
-    // @TODO: Inefficient since 2 db accesses
-    // existsById() might be using a count query
-    // No docs mention this but this is what I think after digging through the source code,
-    // but, if I were you I won't take my word.
-    // someone write proper docs!
     @DeleteMapping(path = "/restaurants/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeRestaurantById(@PathVariable Long id) {
-        if(!restaurantRepository.existsById(id)) {
-            throw new RestaurantNotFoundException(id);
-        }
-        restaurantRepository.deleteById(id);
+        restaurantService.removeById(id);
     }
 
     @PatchMapping(path = "/restaurants/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateRestaurantSeats(@PathVariable Long id, @RequestParam int numberOfSeats) {
-        int changed_records = restaurantRepository.updateRestaurantSeats(id, numberOfSeats);
-        if(changed_records == 0) {
-            throw new RestaurantNotFoundException(id);
-        }
+        restaurantService.updateSeats(id, numberOfSeats);
     }
 
     @PutMapping(path = "/restaurants/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void updateRestaurant(@PathVariable Long id, @RequestBody Restaurant restaurant) {
-        restaurant.setId(id);
-        restaurantProducerService.handleAddNewRestaurant(restaurant);
-    }
-
-    private Restaurant getRestaurantByIdOrThrow(Long id) throws RestaurantNotFoundException {
-        return restaurantRepository.findById(id)
-                .orElseThrow(() -> new RestaurantNotFoundException(id));
+        restaurantService.update(id, restaurant);
     }
 }
