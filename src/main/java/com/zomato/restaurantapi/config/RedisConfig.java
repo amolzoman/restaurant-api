67c92cf2999567744d1a6e46ee950729fd2c9221
Amol Grover <amol.grover@zomato.com>
2021-02-10 11:16:19 +0530

    Implement generic redis template
    This generic template could be used to create multiple
    templates with different value structure.

--- src/main/java/com/zomato/restaurantapi/config/RedisConfig.java
+++ src/main/java/com/zomato/restaurantapi/config/RedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -33,10 +33,14 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Restaurant> redisTemplate() {
-        RedisTemplate<String, Restaurant> redisTemplate = new RedisTemplate<>();
+        return genericRedisTemplate();
+    }
+
+    private <T> RedisTemplate<String, T> genericRedisTemplate() {
+        RedisTemplate<String, T> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Restaurant.class));
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
 }
